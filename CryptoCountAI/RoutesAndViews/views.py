5c7cc8112487b152f0394c5ad9082c797eeca856
Henrik <54423408+HenrikMoe@users.noreply.github.com>
2023-03-06 18:16:00 -0800

    Update views.py

--- CryptoCountAI/RoutesAndViews/views.py
+++ CryptoCountAI/RoutesAndViews/views.py
@@ -6,4 +6,18 @@ def hello_world(request):
 
 
 
+import tensorflow as tf
 
+def typeModelLayer1(request):
+    # load the model
+    model = tf.keras.models.load_model('my_model.h5')
+
+    #load the transaction pipeline
+    
+    # evaluate the model
+    #loss, accuracy = model.evaluate(x_test, y_test)
+
+    # make predictions
+    predictions = model.predict(transaction)
+    
+    
